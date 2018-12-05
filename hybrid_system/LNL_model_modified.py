18fb0db1fb3b0275e1f6de39b915f79459d0fd96
Joao Victor Campos Moraes <jvcm@up1c03.windows.cin.ufpe.br>
2018-12-05 20:35:45 -0300

    possivel defasagem

--- hybrid_system/LNL_model_modified.py
+++ hybrid_system/LNL_model_modified.py
@@ -3,7 +3,6 @@ from sklearn.metrics import mean_squared_error
 import math
 import random
 import functions
-from sklearn.linear_model import Perceptron
 
 class LNL_ANN:
 	def __init__(self, m = 5, z = 2): # Rede da série (m)/ Rede do ruído (z)
@@ -15,12 +14,12 @@ class LNL_ANN:
 
 	def forward_series(self, weight, X):
 		m = int((len(weight) - 4)/3)
-		w1 = weight[: m]
+		w1 = weight[:m]
 		b1 = weight[m]
-		w2 = weight[m+1: 2*m+1]
-		b2 = weight[2*m+1: 3*m+1]
-		w3 = weight[3*m+1: 3*m+3]
-		b3 = weight[3*m+3]
+		w2 = weight[m+1:2*m+1]
+		b2 = weight[2*m+1:3*m+1]
+		w3 = weight[-3:-1]
+		b3 = weight[-1]
 		#Calculate Net1 and Net2
 		net1 = np.dot(X, w1) + b1
 		net2 = np.prod(X*w2 + b2)
@@ -42,78 +41,12 @@ class LNL_ANN:
 		net3 = w3[0]*f1 + w3[1]*f2 + b3
 		# print(net3)
 		return net3
-
-	def _fit(self, X, y, d = 30, c1i = 2.0, c1f = 3.0, c2i = 2.0, c2f = 3.0,
-		w1 = 0.1, w2 = 1.0, maxt = 1000):
-		m = X.shape[1]
-		k = 3*m + 4
-		#MPSO Algorythm
-
-		particles = np.random.rand(d, k)
-		velocity = np.random.uniform(low = -1.0, high = 1.0, size = (d, k))
-		pBest = particles[:]
-		gBest = particles[0]
-		best_fitness = np.full(d, np.inf)
-
-		for t in range(maxt):
-			fitness = np.zeros(d)
-			for i, p in enumerate(particles):
-				output = np.zeros(len(y))
-				for j, x in enumerate(X):
-					output[j] = self.forward_series(p, x)
-				fitness[i] = mean_squared_error(y, output)
-				if fitness[i] < best_fitness[i]:
-					pBest[i] = p[:]
-					best_fitness[i] = fitness[i]
-			# print(best_fitness)
-			gBest = particles[np.argmin(fitness)]
-			bad_index = np.argmax(fitness)
-			c1 = ((c1f - c1i)*t/maxt) +c1i
-			c2 = ((c2f - c2i)*t/maxt) +c2i
-			w = w1 + (w2 - w1)*((maxt - t)/maxt)
-			for i, p in enumerate(particles):
-				if i == bad_index:
-					velocity[i] = np.random.uniform(low = -1.0, high = 1.0, size = k)
-					particles[i] = np.random.rand(k)
-					pBest[i] = particles[i]
-				else:
-					velocity[i] = w*velocity[i] + c1*random.uniform(0, 1)*(pBest[i] - p) + c2*random.uniform(0, 1)*(gBest - p)
-					particles[i] = p +velocity[i]
-
-		#Optimal solution. The L&NL-ANN weights will be gBest
-		return gBest
 	
 	def fit_MPSO(self, X, y, d = 30, c1i = 2.0, c1f = 3.0, c2i = 2.0, c2f = 3.0,
 		w1 = 0.5, w2 = 1.0, maxt = 1000):
-# 1st Layer: Series Forecasting
-		weight_series = self._fit(X, y, d = d, c1i = c1i, c1f = c1f, c2i = c2i, c2f = c2f,
-		w1 = w1, w2 = w2, maxt = maxt)
 
-		predict = np.zeros(len(y))
-		for i, x in enumerate(X):
-			predict[i] = self.forward_series(weight_series, x)
-
-		# Residual data set
-		resid_train = functions._error(y, predict)
-		resid_train_lags = functions.gerar_janelas(tam_janela = self.z, serie = resid_train)
-		X_error_train = resid_train_lags[:, :-1]
-		y_error_train = resid_train_lags[:, -1]
-
-# 2nd Layer: Residual Forecasting
-		weight_residual = self._fit(X_error_train, y_error_train, d = d, c1i=c1i-1, c1f=c1f-1, c2i=c2i-1, c2f=c2f-1, w1= w1-0.2, w2=w2-0.2, maxt = maxt)
-		
-		predict_res = np.zeros(len(y_error_train))
-		for i, x in enumerate(X_error_train):
-			predict_res[i] = self.forward_series(weight_residual, x)
-
-		# Super particle
-		self.weight[:self.k - 3] = np.append(weight_series, weight_residual)
-# 3rd Layer: Combination
-		X_comb = np.hstack((predict[self.z + 1:].reshape(-1,1), predict_res.reshape(-1,1)))
-		X_comb = np.hstack((X_comb, np.ones(len(X_comb)).reshape(-1,1)))
-
-		particles = np.random.rand(d, 3)
-		velocity = np.random.uniform(low = -1.0, high = 1.0, size = (d, 3))
+		particles = np.random.rand(d, self.k)
+		velocity = np.random.uniform(low = -1.0, high = 1.0, size = (d, self.k))
 		pBest = particles[:]
 		gBest = particles[0]
 		best_fitness = np.full(d, np.inf)
@@ -121,14 +54,35 @@ class LNL_ANN:
 		for t in range(maxt):
 			fitness = np.zeros(d)
 			for i, p in enumerate(particles):
-				output = np.zeros(len(y_error_train))
+# 1st Layer: Series Forecasting
+				predict = np.zeros(len(y))
+				for j, x in enumerate(X):
+					predict[j] = self.forward_series(p[: 3*self.m+4], x)
+
+				# Residual data set
+				resid_train = functions._error(y, predict)
+				resid_train_lags = functions.gerar_janelas(tam_janela = self.z - 1, serie = resid_train)
+				X_error_train = resid_train_lags
+
+# 2nd Layer: Residual Forecasting
+		
+				predict_res = np.zeros(len(X_error_train))
+				for j, x in enumerate(X_error_train):
+					predict_res[j] = self.forward_series(p[3*self.m+4 : 3*self.m+4 + 3*self.z+4], x)
+
+		# Super particle
+# 3rd Layer: Combination
+				X_comb = np.hstack((predict[self.z:].reshape(-1,1), predict_res.reshape(-1,1)))
+				X_comb = np.hstack((X_comb, np.ones(len(X_comb)).reshape(-1,1)))
+
+				output = np.zeros(len(X_error_train))
 				for j, x in enumerate(X_comb):
-					output[j] = np.dot(p, x)
-				fitness[i] = mean_squared_error(y[self.z+1:], output)
+					output[j] = np.dot(p[-3:], x)
+				fitness[i] = mean_squared_error(y[self.z:], output)
+
 				if fitness[i] < best_fitness[i]:
 					pBest[i] = p[:]
 					best_fitness[i] = fitness[i]
-			# print(best_fitness)
 			gBest = particles[np.argmin(fitness)]
 			bad_index = np.argmax(fitness)
 			c1 = ((c1f - c1i)*t/maxt) +c1i
@@ -136,14 +90,14 @@ class LNL_ANN:
 			w = w1 + (w2 - w1)*((maxt - t)/maxt)
 			for i, p in enumerate(particles):
 				if i == bad_index:
-					velocity[i] = np.random.uniform(low = -1.0, high = 1.0, size = 3)
-					particles[i] = np.random.rand(3)
+					velocity[i] = np.random.uniform(low = -1.0, high = 1.0, size = self.k)
+					particles[i] = np.random.rand(self.k)
 					pBest[i] = particles[i]
 				else:
 					velocity[i] = w*velocity[i] + c1*random.uniform(0, 1)*(pBest[i] - p) + c2*random.uniform(0, 1)*(gBest - p)
 					particles[i] = p +velocity[i]
-		weight_combination = gBest[:]
-		self.weight[-3:] = weight_combination
+		
+		self.weight = gBest[:]
 		return
 
 
@@ -159,7 +113,7 @@ class LNL_ANN:
 		pred_resid = np.zeros(len(resid_window))
 		for i, x in enumerate(resid_window):
 			pred_resid[i] = self.forward_series(self.weight[3*self.m+4 : 3*self.m+4 + 3*self.z+4], x)
-		X_comb = np.hstack((series[self.z + 1: ].reshape(-1,1), pred_resid.reshape(-1,1)))
+		X_comb = np.hstack((series[self.z + 1:].reshape(-1,1), pred_resid.reshape(-1,1)))
 		X_comb = np.hstack((X_comb, np.ones(len(X_comb)).reshape(-1,1)))
 		# print(len(X_comb))
 		predict = np.zeros(len(X_comb))
@@ -181,7 +135,7 @@ serie_lags = functions.gerar_janelas(tam_janela = ent, serie = serie_normal)
 X_train, y_train, X_test, y_test = functions.split_serie_with_lags(serie = serie_lags, perc_train = 0.86, perc_val = 0)
 
 model = LNL_ANN(m = ent, z = 2)
-model.fit_MPSO(X_train, y_train, d = 40, c1i = 1.0, c1f = 2.0, c2i = 1.0, c2f = 2.0, w1 = 0.2, w2 = 0.7, maxt = 2000)
+model.fit_MPSO(X_train, y_train, d = 70, c1i = 1.0, c1f = 2.0, c2i = 1.0, c2f = 2.0, w1 = 0.2, w2 = 0.7, maxt = 4000)
 pred = model.predict(X_test)
 pred = functions.desnormalizar(pred, data)
 print('y_test:',data[-len(pred):],'\nprediction:',pred.values.reshape(1,-1))
