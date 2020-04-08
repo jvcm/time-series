74c9a321cf4a737420b16b654932434257967565
jvcm <jvcm@cin.ufpe.br>
2020-04-08 19:57:35 -0300

    beer results

--- hybrid_system/experiments.py
+++ hybrid_system/experiments.py
@@ -19,22 +19,24 @@ from progress.bar import IncrementalBar
 
 ### Dataset ###
 
-name = 'Beer'
+name = 'Lynx'
 df = pd.read_csv('./data/' + name + '.csv')
 data = df.iloc[:, -1].values
+data = np.log(data)
 
 if name not in os.listdir('./results/'):
     os.mkdir('./results/' + name) # Create directory for current dataset
 
 ### Set parameters according to dataset ###
 
-ent = 8 # Janela do modelo normal
-n_exp = 30 # Numero de experimentos
+ent = 4 # Janela do modelo normal
+n_exp = 40 # Numero de experimentos
 tam_jan = 2 # Janela do ruido
+print('Data set: ' + name)
 bar = IncrementalBar('Countdown', max = n_exp)
 
 # Sarima
-order = (0,1,1)
+order = (2,0,0)
 
 #WMES
 s_periods= 4
@@ -59,55 +61,55 @@ for i in range(n_exp):
     
     ############################## Hybrid LNL ANN (Original) ##############################
 
-    # model_lnl = hybrid_ann.LNL_ANN(m = ent)
-    # model_lnl.fit_MPSO(X = X_train, y = y_train, d= 40, c1i = 1.0, c1f = 2.0, c2i = 1.0, c2f = 2.0, w1 = 0.5, w2 = 1.0, maxt = 5000)
-    # y_lnl = model_lnl.predict(X_test)
+    model_lnl = hybrid_ann.LNL_ANN(m = ent)
+    model_lnl.fit_MPSO(X = X_train, y = y_train, d= 40, c1i = 1.0, c1f = 2.0, c2i = 1.0, c2f = 2.0, w1 = 0.5, w2 = 1.0, maxt = 6000)
+    y_lnl = model_lnl.predict(X_test)
         
-    # predictions_lnl['LNL-ANN_' + str(i + 1)] = y_lnl[:]
+    predictions_lnl['LNL-ANN_' + str(i + 1)] = y_lnl[:]
         
         
-    # ############################## Model 1 ##############################
+    ############################## Model 1 ##############################
     
-    # model = hybrid_ann.LNL_ANN(m = ent)
-    # model.fit_MPSO(X = X_train, y = y_train, d= 40, c1i = 1.0, c1f = 2.0, c2i = 1.0, c2f = 2.0, w1 = 0.5, w2 = 1.0, maxt = 4000)
-    # y_pred = model.predict(X_test)
+    model = hybrid_ann.LNL_ANN(m = ent)
+    model.fit_MPSO(X = X_train, y = y_train, d= 40, c1i = 1.0, c1f = 2.0, c2i = 1.0, c2f = 2.0, w1 = 0.5, w2 = 1.0, maxt = 5000)
+    y_pred = model.predict(X_test)
     
-    # y_train_pred = model.predict(X_train)
-    # resid_train = functions._error(y_train, y_train_pred)
-    # resid_test = functions._error(y_test, y_pred)
+    y_train_pred = model.predict(X_train)
+    resid_train = functions._error(y_train, y_train_pred)
+    resid_test = functions._error(y_test, y_pred)
 
-    # resid_train_lags = functions.gerar_janelas(tam_janela = tam_jan, serie = resid_train)
-    # resid_test_lags = functions.gerar_janelas(tam_janela = tam_jan, serie = resid_test)
+    resid_train_lags = functions.gerar_janelas(tam_janela = tam_jan, serie = resid_train)
+    resid_test_lags = functions.gerar_janelas(tam_janela = tam_jan, serie = resid_test)
 
-    # ### Training/test
-    # X_error_train = resid_train_lags[:, :-1]
-    # y_error_train = resid_train_lags[:, -1]
+    ### Training/test
+    X_error_train = resid_train_lags[:, :-1]
+    y_error_train = resid_train_lags[:, -1]
     
-    # X_error_test = resid_test_lags[:, :-1]
-    # y_error_test = resid_test_lags[:, -1]
+    X_error_test = resid_test_lags[:, :-1]
+    y_error_test = resid_test_lags[:, -1]
     
-    # model_resid = hybrid_ann.LNL_ANN(m = tam_jan)
-    # model_resid.fit_MPSO(X = X_error_train, y = y_error_train, d= 40, c1i = 1.0, c1f = 2.0, c2i = 1.0, c2f = 2.0, w1 = 0.2, w2 = 0.8, maxt = 1000)
+    model_resid = hybrid_ann.LNL_ANN(m = tam_jan)
+    model_resid.fit_MPSO(X = X_error_train, y = y_error_train, d= 40, c1i = 1.0, c1f = 2.0, c2i = 1.0, c2f = 2.0, w1 = 0.2, w2 = 0.8, maxt = 1000)
     
-    # resid_pred = model_resid.predict(X_error_test) + y_pred[tam_jan+1:]
+    resid_pred = model_resid.predict(X_error_test) + y_pred[tam_jan+1:]
         
-    # predictions_model1['Model1_' + str(i + 1)] = resid_pred[:]
+    predictions_model1['Model1_' + str(i + 1)] = resid_pred[:]
         
-    # ############################## Model 2 ##############################
+    ############################## Model 2 ##############################
     
-    # X_train_e = np.hstack((X_train[1:, :], resid_train[:-1].reshape(-1, 1)))
-    # X_test_e = np.hstack((X_test[1:, :], resid_test[:-1].reshape(-1,1)))
+    X_train_e = np.hstack((X_train[1:, :], resid_train[:-1].reshape(-1, 1)))
+    X_test_e = np.hstack((X_test[1:, :], resid_test[:-1].reshape(-1,1)))
     
-    # model2 = hybrid_ann.Hybrid_ANN(model.weight)
-    # model2.fit_MPSO(X = X_train_e, y = y_train[1:], d= 40, c1i = 1.0, c1f = 2.0, c2i = 1.0, c2f = 2.0, w1 = 0.2, w2 = 0.8, maxt = 1000)
+    model2 = hybrid_ann.Hybrid_ANN(model.weight)
+    model2.fit_MPSO(X = X_train_e, y = y_train[1:], d= 40, c1i = 1.0, c1f = 2.0, c2i = 1.0, c2f = 2.0, w1 = 0.2, w2 = 0.8, maxt = 1000)
     
-    # y_pred2 = model2.predict(X_test_e)
+    y_pred2 = model2.predict(X_test_e)
         
-    # predictions_model2['Model2_' + str(i + 1)] = y_pred2[:]
+    predictions_model2['Model2_' + str(i + 1)] = y_pred2[:]
         
     ############################## RBF ##############################
     
-    rbf = RBF(ent, 32, 1)
+    rbf = RBF(ent, 20, 1)
     rbf.train(X_train, y_train)
     z = rbf.test(X_test)
     # z = functions.desnormalizar(z, data).iloc[:,0].values
@@ -139,8 +141,8 @@ sarima = np.array(previsoes)
 
 ############################## WMES ##############################
 
-fit2 = ExponentialSmoothing(serie_normal[:ref-2], seasonal_periods = s_periods, trend='add', seasonal='mul').fit(use_boxcox=True)
-wmes = fit2.forecast(len(y_test))
+# fit2 = ExponentialSmoothing(serie_normal[:ref-2], seasonal_periods = s_periods, trend='add', seasonal='mul').fit(use_boxcox=True)
+# wmes = fit2.forecast(len(y_test))
 
 ######################################################################
 ######################################################################
@@ -151,11 +153,11 @@ wmes = fit2.forecast(len(y_test))
 
 predictions_other['Y_true'] = y_test[:]
 predictions_other['SARIMA'] = sarima[:]
-predictions_other['WMES'] = wmes[:]
+# predictions_other['WMES'] = wmes[:]
 
 predictions_rbf.to_csv('./results/' + name + '/rbf.csv', index= False)
 predictions_ffann.to_csv('./results/' + name + '/ffann.csv', index= False)
-# predictions_lnl.to_csv('./results/' + name + '/lnl.csv', index= False)
-# predictions_model1.to_csv('./results/' + name + '/model1.csv', index= False)
-# predictions_model2.to_csv('./results/' + name + '/model2.csv', index= False)
+predictions_lnl.to_csv('./results/' + name + '/lnl.csv', index= False)
+predictions_model1.to_csv('./results/' + name + '/model1.csv', index= False)
+predictions_model2.to_csv('./results/' + name + '/model2.csv', index= False)
 predictions_other.to_csv('./results/' + name + '/other.csv', index= False)
